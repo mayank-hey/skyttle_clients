b94e8ebe4e351746c6768cb3e59866a7ad0d2b9b
Viktor Pekar <v.pekar@gmail.com>
2013-12-06 12:22:43 +0000

    m

--- skyttle-client.py
+++ skyttle-client.py
@@ -8,8 +8,7 @@ URL = "https://sentinelprojects-skyttle20.p.mashape.com/"
 text = "We have visited this restaurant a few times in the past, and the meals have been ok, but this time we were deeply disappointed."
 
 opener = urllib2.build_opener(urllib2.HTTPHandler)
-params = {'text': text, 'lang': 'en', 'keywords': 1, 'sentiment': 1,
-          'annotate': 0}
+params = {'text': text, 'lang': 'en', 'keywords': 1, 'sentiment': 1}
 headers = {'X-Mashape-Authorization': YOUR_MASHAPE_KEY}
 
 request = urllib2.Request(URL, urllib.urlencode(params), headers=headers)
